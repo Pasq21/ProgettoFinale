1809ab46d17919fa442abd6064805f9d34c5a392
robertobelmonte1 <roberto_belmonte@hotmail.it>
2019-07-26 12:01:11 +0200

    modifica databse

--- src/main/java/it/dstech/course/connection/Database.java
+++ src/main/java/it/dstech/course/connection/Database.java
@@ -12,25 +12,29 @@ public class Database {
 	private static final String DB_URL = "jdbc:mysql://localhost/world?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	private static final String USER = "root";
 	private static final String PASS = "dstech";
-	
-	
+
+
 	public static boolean checkUser (String username) throws SQLException, ClassNotFoundException {
 		Class.forName(JDBC_DRIVER);	
 		Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-		/* Statement createStatement = conn.createStatement();
-		ResultSet execute = createStatement.executeQuery(query); */
+
 		return false;
 	}
-	
-	public static void addUser (marito) throws SQLException, ClassNotFoundException {
+
+	public static void addUser (Marito marito) throws SQLException, ClassNotFoundException {
 		Class.forName(JDBC_DRIVER);	
 		Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-		/* Statement createStatement = conn.createStatement();
-		ResultSet execute = createStatement.executeQuery(query); */
-		return false;
+
+
 	}
-	
-	
-	
-	
+
+	public static boolean checkLogin (String username, String password) throws SQLException, ClassNotFoundException {
+		Class.forName(JDBC_DRIVER);	
+		Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+		return false;
+
+
+	}
+
 }
