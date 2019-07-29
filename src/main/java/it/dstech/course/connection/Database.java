3d53a4ec00ee646fe2cf65b7388662d5ffe1592f
Fab91-r <fabiano.ruggeri91@gmail.com>
2019-07-29 16:54:22 +0200

    modifica Database

--- src/main/java/it/dstech/course/connection/Database.java
+++ src/main/java/it/dstech/course/connection/Database.java
@@ -21,7 +21,7 @@ import it.dstech.course.model.Marito;
 public class Database {
 
 	private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String DB_URL = "jdbc:mysql://localhost/mogliemiglia?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	private static final String DB_URL = "jdbc:mysql://192.168.2.96:3306/mogliemiglia?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	private static final String USER = "root";
 	private static final String PASS = "dstech";
 
